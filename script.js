7a942006cc3923bac74a94e1a632d51c18ccdc48
Luka <luka.luka.luka93@gmail.com>
2024-10-30 16:42:48 -0700

    from date i pulled out year, month, day and time

--- script.js
+++ script.js
@@ -16,18 +16,23 @@ const wind = document.querySelector('.wind');
 
 let cityInput = 'Los Angeles'
 
-async function fetchWeather () {
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=2dff0dc594da402c997192633242706&q=${cityInput}`);
-    const data = await response.json();
-    console.log(data);
+function fetchWeather () {
+    fetch(`http://api.weatherapi.com/v1/current.json?key=2dff0dc594da402c997192633242706&q=${cityInput}`)
+    .then(response => response.json()) 
+    .then(data => {
+        console.log(data)
+
+        temp.innerHTML = data.current.temp_f;
+        weatherCondition.innerHTML = data.current.condition.text;
+
+        const date = data.location.localtime;
+        const year = parseInt(date.substr(0, 4))
+        const month = parseInt(date.substr(5, 2));
+        const day = parseInt(date.substr(8, 2));
+        const time = date.substr(11);
+    })
 }
 fetchWeather();
 
-function handleInput () {
-    cityInput = search.value;
-}
-function handleSearch () {
-    fetchWeather();
-}
-search.addEventListener('input', handleInput);
-button.addEventListener('.click', handleSearch);
+
+
