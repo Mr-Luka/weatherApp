dd72b809ab34a13cc60240c937574cbdc90e595c
Luka <luka.luka.luka93@gmail.com>
2024-10-30 15:24:30 -0700

    selected all the elements

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-
+const weatherBackground = document.querySelector('.weather-app');
+const temp = document.querySelector('.temp');
+const cityName = document.querySelector('.name');
+const time = document.querySelector('.time');
+const date = document.querySelector('.date');
+const weatherIcon = document.querySelector('.icon img');
+const weatherCondition = document.querySelector('.condition');
+const form = document.querySelector('#locationInput');
+const search = document.querySelector('.search'); 
+const button = document.querySelector('.submit');
+const cities = document.querySelector('.city');
+const cloudy = document.querySelector('.cloud');
+const humidity = document.querySelector('.humidity');
+const wind = document.querySelector('.wind');
 
 
 let cityInput = 'Los Angeles'
